2ba1ff7e7a1863fa987b063cc4bfde8e3289508b
minyeok <alsgurgur@naver.com>
2025-02-17 09:40:34 +0900

    morning coding

--- programmers/solving/sol.py
+++ programmers/solving/sol.py
@@ -1,13 +1,3 @@
-def solution(dot):
-    answer = 0
-    [x, y]] = dot
-
-    if x > 0 and y > 0:
-        answer = 1
-    elif x < 0 and y > 0:
-        answer = 2
-    elif x < 0 and y < 0:
-        answer = 3
-    else:
-        answer = 4
+def solution(money):
+    answer = divmod(money, 5500)
     return answer

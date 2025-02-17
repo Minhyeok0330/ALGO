0fd47fa192af185a4d17e43df2563914459d8b5f
minyeok <alsgurgur@naver.com>
2025-02-17 17:15:38 +0900

    practicing

--- programmers/solving/sol.py
+++ programmers/solving/sol.py
@@ -1,3 +1,8 @@
-def solution(money):
-    answer = divmod(money, 5500)
-    return answer
+def solution(my_string):
+    
+    my_string = my_string.lower()
+    sorted_string = list[my_string]
+    result = sorted_string.sort()
+    return ''.join(result)
+
+print(solution('asdfasdfa'))

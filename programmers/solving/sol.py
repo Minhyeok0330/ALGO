eebcd029f1da1420f9b2a3918bb25002f84fabc4
Minhyeok0330 <alsgurgud@naver.com>
2025-02-18 20:26:22 +0900

    contribution test

--- programmers/solving/sol.py
+++ programmers/solving/sol.py
@@ -1,8 +1,6 @@
-def solution(my_string):
-   answer = []
-    
-    for chrt in my_string:
-        if chrt not in 'aeiou':
-            answer.append(chrt)
-        
-    return ('',join.(answer))
+def solution(my_string, n):
+    answer = ''
+    return answer
+
+print()
+new
